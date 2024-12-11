3566425b1f81032797c8b7274aa5a8027bcd2e6d
Yatika Jena <j.yatika@iitg.ac.in>
2024-12-12 00:22:23 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
-import {View} from 'react-native'
+import { View,Text, StyleSheet } from "react-native";
 
 export default function App(){
-  return <View>
-  </View>;
+  return (
+    <View style={styles.container}>
+    <Text>StyleSheet API</Text>
+    </View>
+  )
 }
+
+const styles= StyleSheet.create({
+  container:{
+    flex:1, backgroundColor:"plum", padding: 60
+  }}
+)
