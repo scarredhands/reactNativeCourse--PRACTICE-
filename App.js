f8c04215cd8a14ffab218d3d3b396cc682f90126
Yatika Jena <j.yatika@iitg.ac.in>
2024-12-12 20:52:15 +0530

    learned lists and flatlists

--- App.js
+++ App.js
@@ -1,15 +1,47 @@
-import { View,Text, StyleSheet } from "react-native";
-
+import { View,Text, StyleSheet, ScrollView } from "react-native";
+import { SafeAreaView, StatusBar, FlatList } from "react-native";
+import pokemonList from './data.json';
 export default function App(){
   return (
-    <View style={styles.container}>
-    <Text>StyleSheet API</Text>
-    </View>
+    <SafeAreaView style= {styles.container}>
+    <FlatList
+     data={pokemonList}
+     renderItem={({item})=>{
+        return (
+               <View key={item.id}>
+                 <Text>{item.type}</Text>
+                 <Text>{item.name}</Text>
+               </View>
+             )
+     }}
+     ListHeaderComponent={<Text >Pokemon List</Text>}>
+     
+    </FlatList>
+    </SafeAreaView>
+    // <ScrollView style={[styles.container, styles.ScrollView]}>
+    // {
+    //   pokemonList.map(pokemon=>{
+    //     return (
+    //       <View key={pokemon.id}>
+    //         <Text>{pokemon.type}</Text>
+    //         <Text>{pokemon.name}</Text>
+    //       </View>
+    //     )
+    //   })
+    // }
+
+    // </ScrollView>
   )
 }
 
 const styles= StyleSheet.create({
   container:{
-    flex:1, backgroundColor:"plum", padding: 60
-  }}
+    flex:1, backgroundColor:"plum",
+     paddingTop: StatusBar.currentHeight,
+     
+  },
+  ScrollView: {
+    paddingHorizontal: 20
+  }
+}
 )
